018f3554d96f0d97459b70c5351a439cdb24928a
Sihem Lerari <cs_lerari@outlook.com>
2024-10-05 23:37:28 +0100

    replace scaffolding assertions now that we have equality

--- test/add_fractions_test.dart
+++ test/add_fractions_test.dart
@@ -3,34 +3,30 @@ import 'package:test/test.dart';
 
 void main() {
   test('zero plus zero', () {
-    expect(Fraction(0).plus(Fraction(0)).intValue(), 0);
+    expect(Fraction(0).plus(Fraction(0)), equals(Fraction(0)));
   });
 
   test('non-zero plus zero', () {
-    expect(Fraction(2).plus(Fraction(0)).intValue(), 2);
+    expect(Fraction(2).plus(Fraction(0)), equals(Fraction(2)));
   });
 
   test('zero plus non-zero', () {
-    expect(Fraction(0).plus(Fraction(5)).intValue(), 5);
+    expect(Fraction(0).plus(Fraction(5)), equals(Fraction(5)));
   });
 
   test('non-zero non-negative integers', () {
-    expect(Fraction(3).plus(Fraction(5)).intValue(), 8);
+    expect(Fraction(3).plus(Fraction(5)), equals(Fraction(8)));
   });
 
   test('non-zero negative integers', () {
-    expect(Fraction(-2).plus(Fraction(-3)).intValue(), -5);
+    expect(Fraction(-2).plus(Fraction(-3)), equals(Fraction(-5)));
   });
 
   test('same denominator of 1', () {
-    final Fraction sum = Fraction(2, 1).plus(Fraction(3, 1));
-    expect(sum.numerator, 5);
-    expect(sum.denominator, 1);
+    expect(Fraction(2, 1).plus(Fraction(3, 1)), equals(Fraction(5, 1)));
   });
 
   test('same denominator but different than 1', () {
-    final Fraction sum = Fraction(3, 7).plus(Fraction(5, 7));
-    expect(sum.numerator, 8);
-    expect(sum.denominator, 7);
+    expect(Fraction(3, 7).plus(Fraction(5, 7)), equals(Fraction(8, 7)));
   });
 }
