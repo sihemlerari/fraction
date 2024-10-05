e9c397748773fc623ba03d70a552efd39a165472
Sihem Lerari <cs_lerari@outlook.com>
2024-10-05 23:17:15 +0100

    add more equality test

--- test/equal_fractions_test.dart
+++ test/equal_fractions_test.dart
@@ -5,4 +5,20 @@ void main() {
   test('same numerator & denominator', () {
     expect(Fraction(2, 3), equals(Fraction(2, 3)));
   });
+
+  test('different numerators', () {
+    expect(Fraction(1, 5), isNot(equals(Fraction(3, 5))));
+  });
+
+  test('different denominators', () {
+    expect(Fraction(1, 3), isNot(equals(Fraction(1, 4))));
+  });
+
+  test('whole integer equals same fraction', () {
+    expect(Fraction(9, 1), equals(Fraction(9)));
+  });
+
+  test('whole integer not equal to different whole integer', () {
+    expect(Fraction(4), isNot(equals(Fraction(5))));
+  });
 }
