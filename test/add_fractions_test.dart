7eca6fe97b080d375cfbf6fa838ea5d7934487a2
Sihem Lerari <cs_lerari@outlook.com>
2024-10-05 20:53:42 +0100

    remove duplication in tests

--- test/add_fractions_test.dart
+++ test/add_fractions_test.dart
@@ -3,27 +3,22 @@ import 'package:test/test.dart';
 
 void main() {
   test('zero plus zero', () {
-    Fraction sum = Fraction(0).plus(Fraction(0));
-    expect(sum.intValue(), 0);
+    expect(Fraction(0).plus(Fraction(0)).intValue(), 0);
   });
 
   test('non-zero plus zero', () {
-    Fraction sum = Fraction(2).plus(Fraction(0));
-    expect(sum.intValue(), 2);
+    expect(Fraction(2).plus(Fraction(0)).intValue(), 2);
   });
 
   test('zero plus non-zero', () {
-    Fraction sum = Fraction(0).plus(Fraction(5));
-    expect(sum.intValue(), 5);
+    expect(Fraction(0).plus(Fraction(5)).intValue(), 5);
   });
 
   test('non-zero non-negative integers', () {
-    Fraction sum = Fraction(3).plus(Fraction(5));
-    expect(sum.intValue(), 8);
+    expect(Fraction(3).plus(Fraction(5)).intValue(), 8);
   });
 
   test('non-zero negative integers', () {
-    Fraction sum = Fraction(-2).plus(Fraction(-3));
-    expect(sum.intValue(), -5);
+    expect(Fraction(-2).plus(Fraction(-3)).intValue(), -5);
   });
 }
