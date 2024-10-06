118f01a14fe4f1f82871abc5bcc3e455a9c9ae43
Sihem Lerari <cs_lerari@outlook.com>
2024-10-06 18:29:28 +0100

    explore GCD algorithm from wikipedia, found that it may return negative gcd

--- test/greatest_common_divisor_test.dart
+++ test/greatest_common_divisor_test.dart
@@ -1,8 +1,34 @@
 import 'package:test/test.dart';
 
 void main() {
-  test('greatest common divisor of 1/1 is 1', () {
+  test('reflexive', () {
     expect(gcd(1, 1), equals(1));
+    expect(gcd(5, 5), equals(5));
+  });
+
+  test('relatively prime', () {
+    expect(gcd(1, 3), equals(1));
+    expect(gcd(7, 5), equals(1));
+    expect(gcd(-2, -3), equals(1));
+  });
+
+  test('one is multiple of the other', () {
+    expect(gcd(9, 3), equals(3));
+    expect(gcd(16, 4), equals(4));
+  });
+
+  test('common factor', () {
+    expect(gcd(6, 8), equals(2));
+    expect(gcd(18, 48), equals(6));
+    expect(gcd(24, 28), equals(4));
+    expect(gcd(-24, 28), equals(4));
+    expect(gcd(24, -28), equals(4));
+    expect(gcd(-24, -28), equals(4));
+  });
+
+  test('negatives', () {
+    expect(gcd(-2, -2), equals(-2));
+    expect(gcd(-7, -7), equals(-7));
   });
 }
 
