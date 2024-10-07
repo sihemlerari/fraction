1d65fc0161180dd8821e982f929746dd06c5024b
Sihem Lerari <cs_lerari@outlook.com>
2024-10-07 12:17:21 +0100

    add a handful more tests for adding fractions, found that constructor does not yet avoid negative denominators

--- test/add_fractions_test.dart
+++ test/add_fractions_test.dart
@@ -33,4 +33,29 @@ void main() {
   test('different denominators without reducing', () {
     expect(Fraction(2, 3).plus(Fraction(1, 4)), equals(Fraction(11, 12)));
   });
+
+  test('reduce result to whole number', () {
+    expect(Fraction(1, 2).plus(Fraction(1, 2)), equals(Fraction(1)));
+  });
+
+  test('reduce result even when denominators are the same', () {
+    expect(Fraction(3, 4).plus(Fraction(3, 4)), equals(Fraction(3, 2)));
+  });
+
+  test('one denominator is multiple of the other', () {
+    expect(Fraction(2, 3).plus(Fraction(1, 6)), equals(Fraction(5, 6)));
+  });
+
+  test('common factor in denominators', () {
+    expect(Fraction(1, 6).plus(Fraction(4, 9)), equals(Fraction(11, 18)));
+  });
+
+  test('negative fractions and reducing', () {
+    expect(Fraction(-1, 4).plus(Fraction(3, 4)), equals(Fraction(1, 2)));
+    expect(Fraction(-1, 2).plus(Fraction(3, 8)), equals(Fraction(-1, 8)));
+  });
+
+  test('negative signs everywhere', () {
+    expect(Fraction(1, -4).plus(Fraction(-3, -4)), equals(Fraction(1, 2)));
+  }, skip: 'constructor does not yet avoid negative denominators');
 }
