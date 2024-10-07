3e25af9f46d959af7bda3e09f3b49620d3a9f234
Sihem Lerari <cs_lerari@outlook.com>
2024-10-07 15:28:54 +0100

    add example to demonstrate how to use the package

--- example/fraction_example.dart
+++ example/fraction_example.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:fraction/fraction.dart';
+
+void main() {
+  final Fraction sum1 = Fraction(1, 6).plus(Fraction(4, 9));
+  final Fraction sum2 = Fraction(2, 3) + Fraction(5, 7);
+  print(sum1);
+  print(sum2);
+  print(sum1 == sum2);
+}
